bed180f9c2f7572c19b2bcdf7734ba5d5bea6f17
Josh-T23 <thaisan2@uwm.edu>
2026-02-16 17:17:37 -0600

    Modified environment
    There is now a path to signup page, login page, and home page.

--- habit-tracker-app/vite.config.js
+++ habit-tracker-app/vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        login: resolve(__dirname, "login.html"),
+        signup: resolve(__dirname, "signup.html"),
+        home: resolve(__dirname, "home.html"),
+      },
+    },
+  },
 })
